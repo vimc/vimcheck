89533e76ffecce65feee4ee423542012344aff25
Pratik Gupte <pratikgupte16@gmail.com>
2026-01-08 16:28:48 +0000

    Update docs, add example WPP data

---
+++ R/eg_wpp.R
@@ -0,0 +1,16 @@
+#' Example of UN-WPP time-series data
+#'
+#' An example of the population estimate data used by VIMC.
+#'
+#' @format ## `eg_wpp`
+#' A data frame with 65,448 rows and 5 columns:
+#' \describe{
+#'   \item{country}{Country name; this is a placeholder name.}
+#'   \item{year}{Year}
+#'   \item{age}{Age}
+#'   \item{gender}{Sex given as three categories, "Male", "Female", or "Both"}.
+#'   \item{value}{Population size}
+#' }
+#' @source Derived from data originally prepared by the United Nations as part
+#' of the World Population Prospects: <https://population.un.org/wpp/>.
+"eg_wpp"
