24f822a76f9e0b3241d267b52fa4f82c0c5c63e7
Pratik Gupte <pratikgupte16@gmail.com>
2026-01-22 15:00:02 +0000

    Add scenario colm to burden example

--- data-raw/eg_burden_template.R
+++ data-raw/eg_burden_template.R
@@ -1,7 +1,19 @@
 ## code to prepare `eg_burden_template` dataset goes here
 
-library(readr)
+eg_burden_template <- readr::read_csv(
+  "inst/extdata/central-burden-template.csv"
+)
 
-eg_burden_template <- read_csv("inst/extdata/central-burden-template.csv")
+# assign dummy cohort size value from random draw
+# 10e6 is the value in `eg_wpp`, allow negative differences
+eg_burden_template$cohort_size <- withr::with_seed(
+  1,
+  rnorm(nrow(eg_burden_template), 10e6, 1e4)
+)
+
+eg_burden_template <- tidyr::crossing(
+  eg_burden_template,
+  scenario = c("disease_rout", "No vaccination")
+)
 
 usethis::use_data(eg_burden_template, overwrite = TRUE)
