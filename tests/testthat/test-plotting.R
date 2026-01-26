3cc1656ed19090e83053266305f46e3e09d3e212
Pratik Gupte <pratikgupte16@gmail.com>
2026-01-26 14:36:32 +0000

    Update tests for fns

--- tests/testthat/test-plotting.R
+++ tests/testthat/test-plotting.R
@@ -8,5 +8,53 @@ test_that("plot_compare_demography() works", {
 
   checkmate::expect_class(p, "ggplot")
 
-  vdiffr::expect_doppelganger("A blank plot", p)
+  vdiffr::expect_doppelganger("plot_demography_alignment", p)
+})
+
+test_that("plot_age_patterns() works", {
+  burden <- eg_burden_template
+  burden <- prep_plot_age(burden)
+
+  # manually set values as template default is NA, prevents ggplot warnings
+  burden$value_millions <- 1.0
+
+  p <- plot_age_patterns(burden, 1)
+
+  checkmate::expect_class(p, "ggplot")
+
+  vdiffr::expect_doppelganger("plot_age_patterns", p)
+})
+
+test_that("plot_global_burden_decades() works", {
+  burden <- eg_burden_template
+  year_max <- 2100
+  burden <- prep_plot_burden_decades(burden, year_max)
+
+  # manually set values as template default is NA, prevents ggplot warnings
+  burden$value_millions <- 1.0
+
+  p <- plot_global_burden_decades(burden, 1)
+
+  checkmate::expect_class(p, "ggplot")
+
+  vdiffr::expect_doppelganger("plot_global_burden_decades", p)
+})
+
+test_that("plot_global_burden_decades() works", {
+  burden <- eg_burden_template
+  burden <- prep_plot_global_burden(burden)
+
+  # NOTE: expected use case is to loop over nested column DFs
+  # set values to a dummy placeholder
+  burden$burden_data[[1]]$value_millions <- 1
+
+  p <- plot_global_burden(
+    burden$burden_data[[1]],
+    burden$burden_outcome[[1]],
+    1
+  )
+
+  checkmate::expect_class(p, "ggplot")
+
+  vdiffr::expect_doppelganger("plot_global_burden", p)
 })
