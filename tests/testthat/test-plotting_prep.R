0b70dfefc42a5593ab05d7f010bb7ef08cf4e101
Pratik Gupte <pratikgupte16@gmail.com>
2026-01-26 14:36:32 +0000

    Update tests for fns

--- tests/testthat/test-plotting_prep.R
+++ tests/testthat/test-plotting_prep.R
@@ -11,33 +11,54 @@ test_that("prep_plot_demography() works ", {
   )
 })
 
-skip("Skipped while tests are written")
 test_that("prep_plot_age() works ", {
-  checkmate::expect_tibble(
-    prep_plot_age(x)
+  burden <- eg_burden_template
+  expected_colnames <- c("scenario", "burden_outcome", "age", "value_millions")
+  x <- prep_plot_age(burden)
+
+  checkmate::expect_tibble(x)
+  checkmate::expect_names(
+    colnames(x),
+    permutation.of = expected_colnames
   )
 })
 
 test_that("prep_plot_burden_decades() works ", {
-  checkmate::expect_tibble(
-    prep_plot_burden_decades(x)
+  burden <- eg_burden_template
+  year_max <- 2100
+  x <- prep_plot_burden_decades(burden, year_max)
+
+  expected_colnames <- c(
+    "scenario",
+    "burden_outcome",
+    "decade_label",
+    "value_millions"
+  )
+  checkmate::expect_tibble(x)
+  checkmate::expect_names(
+    colnames(x),
+    permutation.of = expected_colnames
   )
 })
 
+
 test_that("prep_plot_global_burden() works ", {
-  checkmate::expect_tibble(
-    prep_plot_global_burden(x)
+  burden <- eg_burden_template
+  x <- prep_plot_global_burden(burden)
+
+  expected_colnames <- c("burden_outcome", "burden_data")
+
+  checkmate::expect_tibble(x, types = c("character", "list"))
+  checkmate::expect_names(
+    colnames(x),
+    permutation.of = expected_colnames
   )
 })
 
 test_that("prep_plot_coverage_set() works ", {
-  checkmate::expect_tibble(
-    prep_plot_coverage_set(x)
-  )
+  skip("TODO: Determine where coverage set data is generated")
 })
 
 test_that("prep_plot_fvp() works ", {
-  checkmate::expect_tibble(
-    prep_plot_fvp(x)
-  )
+  skip("TODO: Fix how FVP data is generated")
 })
