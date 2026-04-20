46d3d974131d34d115a658d1cfc87e49f9cbdd63
Pratik Gupte <pratikgupte16@gmail.com>
2026-04-20 16:55:25 +0100

    Add pkg constants

--- R/constants.R
+++ R/constants.R
@@ -48,13 +48,65 @@ colnames_plot_demog_compare <- c(
 )
 
 #' @name constants
+colnames_df_missing_cols <- c(
+  "country_name",
+  "vaccine",
+  "activity_type",
+  "year",
+  "modelling_group"
+)
+
+#' @name constants
+COLNAMES_KEY_PRESSURE_TEST <- c(
+  "country",
+  "country_name",
+  "vaccine",
+  "activity_type",
+  "year",
+  "disease",
+  "modelling_group"
+)
+
+#' @name constants
+COLNAMES_INTEREST_PRESSURE_TEST <- union(
+  COLNAMES_KEY_PRESSURE_TEST,
+  c(
+    "fvps",
+    "target_population",
+    "coverage",
+    "deaths_averted",
+    "dalys_averted",
+    "deaths_averted_rate",
+    "dalys_averted_rate"
+  )
+)
+
+#' @name constants
 EXCLUDED_DISEASES <- c("Hib", "PCV", "Rota", "JE")
 
 #' @name constants
-TOUCHSTONE_OLD <- "201910"
+N_TS_MIN_CHARS <- 6L
 
 #' @name constants
-TOUCHSTONE_NEW <- "202310"
+N_TS_YEAR_CHARS <- 4L
 
 #' @name constants
-TOUCHSTONE_OLD_OLD <- "202110"
+MIN_TS_YEAR <- 2000
+
+#' @name constants
+MAX_TS_YEAR <- 2100
+
+#' @name constants
+MIN_TS_MONTH <- 1
+
+#' @name constants
+MAX_TS_MONTH <- 12
+
+#' @name constants
+DEF_TOUCHSTONE_OLD <- "201910"
+
+#' @name constants
+DEF_TOUCHSTONE_NEW <- "202310"
+
+#' @name constants
+DEF_TOUCHSTONE_OLD_OLD <- "202110"
