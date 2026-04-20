9bab126e74a102103415815f3eef0bf457a14b7b
Pratik Gupte <pratikgupte16@gmail.com>
2026-04-20 17:05:50 +0100

    WIP separate plot prep from plot code

--- R/fn_plotting_pressure_testing.R
+++ R/fn_plotting_pressure_testing.R
@@ -4,8 +4,10 @@
 #' scale_fill_distiller scale_x_continuous scale_y_continuous labs vars
 #' labeller label_wrap_gen
 #'
+#' @keywords internal
+#'
 #' @export
-significant_diff_plot <- function(df, outcome) {
+plot_sig_diff <- function(df, outcome) {
   # retained here as this is a small df and a small operation
   df$label <- glue::glue(
     "{df$country_name} | {df$vaccine} | {df$activity_type} | {df$year}"
@@ -75,15 +77,8 @@ plot_diff <- function(
   return(p)
 }
 
-plot_modelling_group_variation <- function(df2, df3, outc = "deaths") {
-  offset <- 1e-6
-
-  df2 %>%
-    left_join(df3, by = join_by(modelling_group, vaccine)) %>%
-    mutate(adj_outc = !!as.name(paste0(outc, "_averted")) + offset) %>%
-    group_by(vaccine) %>%
-    mutate(mean_outc = weighted.mean(adj_outc, fvps, na.rm = TRUE)) %>%
-    ggplot() +
+plot_modelling_group_variation <- function(df, outcome) {
+  ggplot(df) +
     aes(
       fill = as.character(mod_num),
       x = adj_outc,
@@ -109,7 +104,7 @@ plot_modelling_group_variation <- function(df2, df3, outc = "deaths") {
     labs(
       x = paste0(
         "Burden averted (",
-        ifelse(outc == "dalys", "DALYs", outc),
+        ifelse(outcome == "dalys", "DALYs", outcome),
         ")"
       ),
       y = "Vaccine"
@@ -117,51 +112,11 @@ plot_modelling_group_variation <- function(df2, df3, outc = "deaths") {
 }
 
 
-# Gavi plot - future deaths and DALYS averted, 2021-2024 (current time window Gavi looking at, can be amended)
-plot_vaccine_gavi <- function(df, prev_dat = NULL, outcome = "deaths_averted") {
-  df_cur <- df %>%
-    select(all_of(key_cols), !!outcome) %>%
-    filter(year >= 2021, year <= 2024, disease != "COVID") %>%
-    group_by(disease, year) %>%
-    summarise(
-      yearly_outcome = sum(.data[[outcome]], na.rm = TRUE),
-      .groups = "drop"
-    ) %>%
-    mutate(dataset = as.character(new))
-
-  df_prev <- prev_dat %>%
-    select(all_of(key_cols), !!outcome) %>%
-    filter(year >= 2021, year <= 2024, disease != "COVID") %>%
-    group_by(disease, year) %>%
-    summarise(
-      yearly_outcome = sum(.data[[outcome]], na.rm = TRUE),
-      .groups = "drop"
-    ) %>%
-    mutate(dataset = as.character(old))
-
-  df_combined <- bind_rows(df_cur, df_prev)
-
-  df_diff <- df_cur %>%
-    left_join(
-      df_prev,
-      by = c("disease", "year"),
-      suffix = c("_curr", "_prev")
-    ) %>%
-    mutate(
-      yearly_outcome = yearly_outcome_curr - yearly_outcome_prev,
-      dataset = "Difference"
-    ) %>%
-    select(disease, year, yearly_outcome, dataset)
-
-  df_combined <- bind_rows(df_combined, df_diff)
-
-  df_combined$dataset <- factor(
-    df_combined$dataset,
-    levels = c(as.character(old), "Difference", as.character(new))
-  )
-
+#' Gavi plot - future deaths and DALYS averted, 2021-2024
+#' (current time window Gavi looking at, can be amended)
+plot_vaccine_gavi <- function(df, outcome = "deaths_averted") {
   ggplot(
-    df_combined,
+    df,
     aes(
       x = reorder(disease, yearly_outcome),
       y = yearly_outcome,
@@ -186,100 +141,8 @@ plot_vaccine_gavi <- function(df, prev_dat = NULL, outcome = "deaths_averted") {
 
 ### Gavi Cumulative Plot (modelling group + average)
 plot_cumul <- function(df, outcome, disease_filter) {
-  outcome_cols <- names(df)[str_detect(names(df), paste0("^", outcome, "_"))]
-
-  outcome_sym <- sym(outcome)
-  cum_col <- paste0("cum_", outcome)
-  avg_col <- paste0("avg_", outcome)
-
-  col_old <- paste0(outcome, "_old")
-  col_new <- paste0(outcome, "_new")
-
-  combined2 <- df %>%
-    select(
-      country,
-      country_name,
-      disease,
-      vaccine,
-      activity_type,
-      year,
-      modelling_group,
-      all_of(outcome_cols)
-    ) %>%
-    pivot_longer(
-      cols = all_of(outcome_cols),
-      names_to = "touchstone",
-      values_to = "value"
-    ) %>%
-    mutate(
-      touchstone = str_remove(touchstone, paste0("^", outcome, "_")),
-      touchstone = recode(
-        touchstone,
-        "old" = as.character(old),
-        "new" = as.character(new),
-        .default = touchstone
-      ),
-      touchstone = factor(
-        touchstone,
-        levels = c(as.character(old), as.character(new))
-      )
-    )
-  # Cumulative values by modelling group
-  df_cum <- combined2 %>%
-    filter(disease == disease_filter) %>%
-    group_by(modelling_group, touchstone) %>%
-    complete(year = full_seq(year, 1)) %>%
-    arrange(year) %>%
-    mutate(
-      first_valid = min(year[!is.na(value)]),
-      !!cum_col := ifelse(
-        year < first_valid,
-        NA,
-        cumsum(replace_na(value, 0))
-      )
-    ) %>%
-    select(-first_valid) %>%
-    ungroup() %>%
-    mutate(modelling_group = paste(modelling_group, touchstone, sep = "-"))
-
-  # Model average
-  df_avg <- df_cum %>%
-    group_by(year, touchstone) %>%
-    summarise(
-      !!avg_col := mean(!!sym(cum_col), na.rm = TRUE),
-      n_models = sum(!is.na(!!sym(cum_col))),
-      .groups = "drop"
-    ) %>%
-    filter(n_models >= 1) %>%
-    mutate(modelling_group = paste("Model Average", touchstone, sep = "-"))
-
-  # Combine for plot
-  df_plot <- bind_rows(
-    df_cum %>%
-      select(year, modelling_group, touchstone, value = !!sym(cum_col)),
-    df_avg %>%
-      select(year, modelling_group, touchstone, value = !!sym(avg_col))
-  )
-
-  df_plot <- df_plot %>%
-    group_by(modelling_group) %>%
-    filter(sum(value, na.rm = TRUE) > 0) %>%
-    ungroup() %>%
-    mutate(
-      line_type = ifelse(
-        grepl("Model Average", modelling_group),
-        "dashed",
-        "solid"
-      )
-    )
-
-  if (nrow(df_plot) == 0 || all(df_plot$value == 0)) {
-    message("No non-zero data to plot for ", disease_filter, ". Skipping plot.")
-    return(NULL)
-  }
-
   p <- ggplot(
-    df_plot,
+    df,
     aes(
       x = year,
       y = value,
